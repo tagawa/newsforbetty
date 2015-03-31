1ce1485197a0f76ec5813587ddb0710b30f7107d
David A. Kennedy <me@davidakennedy.com>
2015-03-31 14:46:29 -0400

    Add hidden post titles to read more links for screen reader users. Resolves #14

--- js/newsforbetty.js
+++ js/newsforbetty.js
@@ -10,14 +10,14 @@ $(document).ready(function () {
                 var imgDesc = ($.isArray(item.description) && !!item.description[1]) ? 'Photo: ' + item.description[1] : '';
                 var extLink = ($.isArray(item.link)) ? item.link[0].href : item.link;
                 if (!!item.title) {
-                    $('#news').append('<div class="news-item panel panel-default"><div class="panel-heading"><h2>' + item.title + '</h2></div><div class="panel-body">' + img + '<p>' + desc + '</p><p>' + imgDesc + '</p><div><a href="' + extLink + '" class="btn btn-primary read-more">Read more</a></div></div></div>');
+                    $('#news').append('<div class="news-item panel panel-default"><div class="panel-heading"><h2>' + item.title + '</h2></div><div class="panel-body">' + img + '<p>' + desc + '</p><p>' + imgDesc + '</p><div><a href="' + extLink + '" class="btn btn-primary read-more">Read more <span class="sr-only">about ' + item.title + '</span></a></div></div></div>');
                     console.log(index + ": " + item.title);
                 }
             }
             tmpTitle = item.title;
         });
     }
-    
+
     var sources = {
         'nytusa': {
             'name': 'New York Times (U.S.)',
@@ -26,7 +26,7 @@ $(document).ready(function () {
         'nytint': {
             'name': 'New York Times (World)',
             'url':             "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Frss.nytimes.com%2Fservices%2Fxml%2Frss%2Fnyt%2FInternationalHome.xml%22&format=json&callback="
-        }, 
+        },
         'phillydotcom': {
             'name': 'Philly.com',
             'url':"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Fwww.philly.com/philly_news.rss%22&format=json&callback="
@@ -48,13 +48,13 @@ $(document).ready(function () {
             'url': "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Fwww.nationaljournal.com/%3frss%3D1%22&format=json&callback="
         }
     }
-    
+
     function showSources(){
         $.each(sources, function(index,item){
           $('#sources').append('<button type="button" class="btn btn-lg btn-primary" aria-controls="news" data-source="' + index  + '"> ' + item.name +'</button> ');
         });
     }
-    
+
     function getNews(source) {
         $.ajax({url: sources[source].url,
             success: function(data) {
@@ -69,7 +69,7 @@ $(document).ready(function () {
             type: "get"
         })
     }
-    
+
     function showWaiting(el) {
         $(el).empty().append('<p>Please wait...</p><div class="progress"><div class="progress-bar progress-bar-warning progress-bar-striped active" style="width: 100%"></div></div>');
     }
@@ -80,14 +80,14 @@ $(document).ready(function () {
         var source = $(this).data('source');
         getNews(source);
     });
-    
+
     function setFontSize(fontSize) {
         document.body.className = fontSize;
         if (window.localStorage) {
             window.localStorage['fontSize'] = fontSize;
         }
     }
-    
+
     $('#fontDecrease').on('click', function() {
         var fontSize = (document.body.className === 'fontMed' || document.body.className === 'fontSmall') ? 'fontSmall' : 'fontMed';
         setFontSize(fontSize);
@@ -97,11 +97,11 @@ $(document).ready(function () {
         var fontSize = (document.body.className === 'fontMed' || document.body.className === 'fontLarge') ? 'fontLarge' : 'fontMed';
         setFontSize(fontSize);
     });
-    
+
     if (window.localStorage && window.localStorage['fontSize']) {
         document.body.className = window.localStorage['fontSize'];
     }
-    
+
     function showGreeting() {
         var greeting = 'Hello.';
         var today = new Date();
@@ -113,11 +113,11 @@ $(document).ready(function () {
         } else if (hours >= 18 && hours < 23) {
             greeting = 'Good evening.';
         }
-      
+
         $('#greeting').append(greeting);
     }
-    
+
     showGreeting();
-    
+
     showSources();
 });
