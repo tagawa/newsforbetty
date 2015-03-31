30186d8ab5900a94b702f0a79168cbfd7c2f3bed
David A. Kennedy <me@davidakennedy.com>
2015-03-31 13:56:16 -0400

    Add skip link and skip link focus fix to site. Resolves #13

--- js/newsforbetty.js
+++ js/newsforbetty.js
@@ -1,4 +1,31 @@
 $(document).ready(function () {
+    ( function() {
+        var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+            is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+            is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+
+        if ( ( is_webkit || is_opera || is_ie ) && document.getElementById && window.addEventListener ) {
+            window.addEventListener( 'hashchange', function() {
+                var id = location.hash.substring( 1 ),
+                    element;
+
+                if ( ! ( /^[A-z0-9_-]+$/.test( id ) ) ) {
+                    return;
+                }
+
+                element = document.getElementById( id );
+
+                if ( element ) {
+                    if ( ! ( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
+                        element.tabIndex = -1;
+                    }
+
+                    element.focus();
+                }
+            }, false );
+        }
+    })();
+
     function showItems(itemList, source) {
         $('#news').empty().append('<h1>News from ' + sources[source].name + '</h1>');
         var tmpTitle;
@@ -17,7 +44,7 @@ $(document).ready(function () {
             tmpTitle = item.title;
         });
     }
-    
+
     var sources = {
         'nytusa': {
             'name': 'New York Times (U.S.)',
@@ -26,7 +53,7 @@ $(document).ready(function () {
         'nytint': {
             'name': 'New York Times (World)',
             'url':             "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Frss.nytimes.com%2Fservices%2Fxml%2Frss%2Fnyt%2FInternationalHome.xml%22&format=json&callback="
-        }, 
+        },
         'phillydotcom': {
             'name': 'Philly.com',
             'url':"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Fwww.philly.com/philly_news.rss%22&format=json&callback="
@@ -48,13 +75,13 @@ $(document).ready(function () {
             'url': "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Fwww.nationaljournal.com/%3frss%3D1%22&format=json&callback="
         }
     }
-    
+
     function showSources(){
         $.each(sources, function(index,item){
           $('#sources').append('<button type="button" class="btn btn-lg btn-primary" aria-controls="news" data-source="' + index  + '"> ' + item.name +'</button> ');
         });
     }
-    
+
     function getNews(source) {
         $.ajax({url: sources[source].url,
             success: function(data) {
@@ -69,7 +96,7 @@ $(document).ready(function () {
             type: "get"
         })
     }
-    
+
     function showWaiting(el) {
         $(el).empty().append('<p>Please wait...</p><div class="progress"><div class="progress-bar progress-bar-warning progress-bar-striped active" style="width: 100%"></div></div>');
     }
@@ -80,14 +107,14 @@ $(document).ready(function () {
         var source = $(this).data('source');
         getNews(source);
     });
-    
+
     function setFontSize(fontSize) {
         document.body.className = fontSize;
         if (window.localStorage) {
             window.localStorage['fontSize'] = fontSize;
         }
     }
-    
+
     $('#fontDecrease').on('click', function() {
         var fontSize = (document.body.className === 'fontMed' || document.body.className === 'fontSmall') ? 'fontSmall' : 'fontMed';
         setFontSize(fontSize);
@@ -97,11 +124,11 @@ $(document).ready(function () {
         var fontSize = (document.body.className === 'fontMed' || document.body.className === 'fontLarge') ? 'fontLarge' : 'fontMed';
         setFontSize(fontSize);
     });
-    
+
     if (window.localStorage && window.localStorage['fontSize']) {
         document.body.className = window.localStorage['fontSize'];
     }
-    
+
     function showGreeting() {
         var greeting = 'Hello.';
         var today = new Date();
@@ -113,11 +140,11 @@ $(document).ready(function () {
         } else if (hours >= 18 && hours < 23) {
             greeting = 'Good evening.';
         }
-      
+
         $('#greeting').append(greeting);
     }
-    
+
     showGreeting();
-    
+
     showSources();
 });
